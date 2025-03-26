043bbd07d106350b9fd15752b36217d69b824be4
Chhenghab <chhenghab10@gmail.com>
2025-03-26 23:04:10 +0700

    add footer

---
+++ src/Components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from "../../assets/Logo.png"
+import { MenuFooter } from './MenuFooter'
+import { SocialApp } from './SocialApp'
+import "./footer.css"
+
+export const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <div className='left-side'>
+            <img src={logo} alt="logo" />
+            <p>Copyright © 2025 all Rights reserved</p>
+        </div>
+        <div>
+            <MenuFooter />
+        </div>
+        <div>
+            <SocialApp />
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Setting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigation_baar } from '../Components/navigation-bar/Navigation_baar'
+import { Footer } from '../Components/footer/Footer'
+
+export const Setting = () => {
+  return (
+    <div>
+        <Navigation_baar />
+        
+        <Footer />
+    </div>
+  )
+}
+export default Setting;
