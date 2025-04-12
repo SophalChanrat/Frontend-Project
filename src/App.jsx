28ba86059a6d7649ddc1943fb97fb04f71305017
sonika-tang <sonikatang630@gmail.com>
2025-04-12 17:29:49 +0700

    Series_page finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
 import React from "react";
-import MovieRow from "./Components/Movie_row.jsx";
-import { Navigation_baar } from "./Components/navigation-bar/Navigation_baar.jsx";
-
-const movies = [
-  {
-    id: 1,
-    name: "Violet Evergarden",
-    poster: "",
-    rating: "8.5",
-  },
-  {
-    id: 2,
-    name: "Ponyo",
-    poster: "",
-    rating: "7.8",
-  },
-  {
-    id: 3,
-    name: "Suzume",
-    poster: "",
-    rating: "8.2",
-  },
-  {
-    id: 4,
-    name: "Black Clover",
-    poster: "",
-    rating: "8.0",
-  },
-];
+import SeriesPage from "./Components/Series_page.jsx";
 
 function App() {
   return (
-    <div className="bg-gray-900 min-h-screen p-8">
-      <Navigation_baar />
-      <MovieRow title="Trending Movies" movies={movies} />
-
-    </div>
+      <SeriesPage />
   );
 }
 
